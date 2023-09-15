2308e0120c76d416cd171e1bbc2cf4c8aad8df3c
l0ds4m0n3y <103957548+l0ds4m0n3y@users.noreply.github.com>
2023-09-14 23:03:02 -0700

    Update pictureBlurrer.java

--- pictureBlurrer.java
+++ pictureBlurrer.java
@@ -37,8 +37,8 @@ public class pictureBlurrer{
      * @param dest  the 2D array of where the output of the method will be entered
      */
     private static void blur(double[][] source, double[][] dest) {
-        for (int i = 0; i < source.length; i++) {
-            for (int j = 0; j < source[0].length; j++) {
+        for (int i = 0; i < numColumns; i++) {
+            for (int j = 0; j < numRows; j++) {
                 double[] values = sum3x3(source, i, j);
                 dest[i][j] = values[SUM_TOTAL] / values[TOTAL_NUMBERS_SUMMED];
             }
@@ -68,14 +68,15 @@ public class pictureBlurrer{
         }
 
         //new Frame(imageArray);
+        blurredArray = imageArray;
 
 
-        blur(imageArray, blurredArray);
+        //blur(imageArray, blurredArray);
         for (int i = 0; i < iterations; i++) {
             blur(blurredArray, blurredArray);
         }
         
-        //writeImage(blurredArray, imgInfoToString(), getFileName(file, iterations)); //TODO uncomment this
+        writeImage(blurredArray, imgInfoToString(), getFileName(file, iterations)); //TODO uncomment this
         new Frame(blurredArray);
 
         fileScanner.close();
@@ -100,16 +101,16 @@ public class pictureBlurrer{
     
     /** 
      * The method I use to sum the values around the pixel at (row, col) requires pixels to be present around it, but
-     * it does not work around the edges as the scanner would go out of bounds so by implementing a try-catch method
-     * it lets me sum the remaining pixels around the pixel at (row, col). One problem that arises is that an undesired
+     * it does not work around the edges as the scanner would go out of bounds so by implementing a try catch method
+     * it lets me sum the remaining pixels around the pixel at (row, col). One problem this arises is that an undesired
      * black border would form around the image, to combat this I predetermined the amount of pixels counted to 9 and
-     * by utilizing the same try-catch whenever a pixel would be skipped for being out of bounds the number of pixels
+     * by utilizing the same try catch whenever a pixel would be skipped for being out bounds the number of pixels
      * accounted for in the average would lower by 1 for a more accurate average.
      * 
      * The rest is straightforward with a for loop inside another for loop to sum the pixels starting at the top left corner
      * 
      * 
-     * This method returns two numbers in the form of an array, the first number is the actual sum and the second is the
+     * This method returns two numbers in a form of an array, first number is the actual sum and the second is the
      * amount of pixels to be accounted for when taking the average of the pixels surrounding the pixel at (row, col)
      * 
      * @param img 2D array of the original pgm file
@@ -125,11 +126,10 @@ public class pictureBlurrer{
         double[] values = { 0, 0 };
         double value = 0;
         double numOfnumbersSummed = 9;
-        final int LEFT_MARGIN = 1;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 try {
-                    value += img[row - LEFT_MARGIN + i][col - LEFT_MARGIN + j];
+                    value += img[row - 1 + i][col - 1 + j];
                 } catch (Exception e) {
                     numOfnumbersSummed--;
                 }
