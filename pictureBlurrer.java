4054139d667ce9e41457fbfe995b23c6e830f1de
l0ds4m0n3y <dluna777@outlook.com>
2023-09-18 13:57:18 -0700

    asjgdnajkda

--- pictureBlurrer.java
+++ pictureBlurrer.java
@@ -41,7 +41,11 @@ public class pictureBlurrer{
             }
         }
     }
-
+    
+    /**
+     * @param file file to read
+     * @return double[][] 2D array of all the pixel values
+     */
     public static double[][] pgmToArray(File file) throws FileNotFoundException{
         Scanner fileScanner = new Scanner(file);
         magicNumber = fileScanner.nextLine();
@@ -62,6 +66,7 @@ public class pictureBlurrer{
     /**
      * main method that runs the blur method by reading a pgm file data into memory via 2D array and writes a new pgm file 
      * indicating how many iterations of the blur method were passed through 
+     * @param iterations how many times the blur() method will be ran
      * @throws IOException
      */
     public static void runProgram(int iterations) throws IOException {
@@ -106,39 +111,17 @@ public class pictureBlurrer{
 
     
     /** 
-     * The method I use to sum the values around the pixel at (row, col) requires pixels to be present around it, but
-     * it does not work around the edges as the scanner would go out of bounds so by implementing a try catch method
-     * it lets me sum the remaining pixels around the pixel at (row, col). One problem this arises is that an undesired
-     * black border would form around the image, to combat this I predetermined the amount of pixels counted to 9 and
-     * by utilizing the same try catch whenever a pixel would be skipped for being out bounds the number of pixels
-     * accounted for in the average would lower by 1 for a more accurate average.
-     * 
-     * The rest is straightforward with a for loop inside another for loop to sum the pixels starting at the top left corner
-     * 
-     * 
-     * This method returns two numbers in a form of an array, first number is the actual sum and the second is the
-     * amount of pixels to be accounted for when taking the average of the pixels surrounding the pixel at (row, col)
-     * 
      * @param img 2D array of the original pgm file
      * @param row row number of the pixel which the method will anchor from 
      * @param col column number of the pixel which the method will anchor from 
      * 
-     * @return two values, the first number is the final sum of all the pixels around and in pixel of coordinates col, row, the second is
-     * the number of pixels that were summed, typically 9 but in edge cases the number lowers as to avoid a black border
-     * forming. Use constants pictureBlurrer.SUM_TOTAL and pictureBlurrer.TOTAL_NUMBERS_SUMMED for easy distinguishment
+     * @return the average pixel value of the pixels surrounding (row, col)
      */
     
     private static double sum3x3(double[][] img, int row, int col) {
         double currSum = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-
-                /*
-                 * Instead of doing a try-catch here because it will step outside of the bounds, have the numbers loop around
-                 * For example, when you're trying to get the sum of the 9x9 on the left most edge (position 0) it would, instead of just not adding to the sum, go to the position on the other side
-                 * In a 100x100 grid, it would wrap from 0 to position 99. Same with the vertical positions.
-                 * 
-                 */
                 currSum += img[(row - 1 + i + numRows) % numRows][(col - 1 + j + numColumns) % numColumns];
             }
         }
